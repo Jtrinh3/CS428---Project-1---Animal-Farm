69f7170aa980fd378fc19277233e2da9798bed99
Omar <oalkha2@uic.edu>
2019-09-20 13:42:24 -0500

    Added video functionality

--- Assets/ButtonHandlers.cs
+++ Assets/ButtonHandlers.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Video;
 public class ButtonHandlers : MonoBehaviour
 {
     private GameObject UI_Container;
+    private GameObject Video1;
+    private GameObject Video2;
+
     public void menuRotateCounterClockwise()
     {
         UI_Container = GameObject.Find("UI Container");
@@ -15,5 +18,18 @@ public class ButtonHandlers : MonoBehaviour
     {
         UI_Container = GameObject.Find("UI Container");
         UI_Container.transform.Rotate(0, 90, 0, Space.World); //rotate relative to world
+    }
+    public void PlayVideoOne()
+    {
+        Video1 = GameObject.Find("Video1");
+        VideoPlayer player = (VideoPlayer)Video1.GetComponent("VideoPlayer");
+        player.Play();
+    }
+
+    public void PlayVideoTwo()
+    {
+        Video2 = GameObject.Find("Video2");
+        VideoPlayer player = (VideoPlayer)Video2.GetComponent("VideoPlayer");
+        player.Play();
     }
 }
