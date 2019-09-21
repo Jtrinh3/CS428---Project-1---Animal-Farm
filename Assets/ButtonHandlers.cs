e44f81a2ae6706053585cd3f5feb7f0259256e23
Omar <oalkha2@uic.edu>
2019-09-20 21:56:36 -0500

    added functionality for reviews

--- Assets/ButtonHandlers.cs
+++ Assets/ButtonHandlers.cs
@@ -8,6 +8,10 @@ public class ButtonHandlers : MonoBehaviour
     private GameObject UI_Container;
     private GameObject Video1;
     private GameObject Video2;
+    private int reviewNum = 1;
+    private static int TOTAL_REVIEWS = 2;
+
+
 
     public void menuRotateCounterClockwise()
     {
@@ -32,4 +36,60 @@ public class ButtonHandlers : MonoBehaviour
         VideoPlayer player = (VideoPlayer)Video2.GetComponent("VideoPlayer");
         player.Play();
     }
+
+    public void prevReview()
+    {
+        if (reviewNum == 1)
+        {
+            reviewNum = TOTAL_REVIEWS;
+        }
+        else
+        {
+            reviewNum = reviewNum - 1;
+        }
+
+        changeReview(reviewNum);
+
+
+
+    }
+
+    public void nextReview()
+    {
+        if(reviewNum == TOTAL_REVIEWS)
+        {
+            reviewNum = 1;
+        }
+        else
+        {
+            reviewNum = reviewNum + 1;
+        }
+        changeReview(reviewNum);
+
+    }
+
+    public void changeReview(int reviewNumber)
+    {
+        Material gold = Resources.Load("Gold Star", typeof(Material)) as Material;
+        Material black = Resources.Load("Black Star", typeof(Material)) as Material;
+
+    GameObject author = GameObject.Find("Author Text");
+
+        author.GetComponent<UnityEngine.UI.Text>().text = "Debbie D.";
+        GameObject review = GameObject.Find("Review Text");
+
+        review.GetComponent<UnityEngine.UI.Text>().text = "Animal farm is a great book that plays around with many themes. I hope one day my kids grow up to be a strong leader like Napoleon.";
+
+        GameObject star1 = GameObject.Find("star1");
+        GameObject star5 = GameObject.Find("star5");
+        //star1.GetComponent<MeshRenderer>().material = star5.GetComponent<MeshRenderer>().material;
+        star1.GetComponentInChildren<MeshRenderer>().material = star5.GetComponentInChildren<MeshRenderer>().material;
+
+    }
+
+    public void starChange(int stars)
+    {
+
+    }
+
 }
